3f8dfc8f45d3befcb2f71955ab365adb31b45860
Ambuj Kumar <itsambuja@gmail.com>
2018-09-30 17:46:27 +0530

    students list added

---
+++ CMS/views/manager/students/students.php
@@ -0,0 +1,43 @@
+<?php
+    echo '<a href="../students"><h3>back</h3></a>';
+    include '../../../settings/db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Students list</title>
+</head>
+<body>
+    <div>
+        <table border>
+            <tr><th>s.no</th><th>student_id</th><th>Students name</th><th>gender</th><th>mobile</th><th>Department</th></tr>
+            <?php
+                # get department list from database.
+                $sql = "SELECT * FROM students";
+                $res_set = mysqli_query($conn,$sql);
+
+                if($res_set){
+                    if(mysqli_num_rows($res_set) > 0){
+                        while($row = mysqli_fetch_assoc($res_set)){
+                            echo '<tr> 
+                                <td>'.$row['id'] .'</td> 
+                                <td>'.$row['student_id'] .'</td> 
+                                <td>'.$row['first_name'].' '.$row['last_name'].'</td>
+                                <td>'.$row['gender'].'</td>
+                                <td>'.$row['mobile'].'</td>
+                                <td>'.$row['dept'].'</td>
+                                </tr>';
+                        }
+                    }else{
+                        echo 'wrong query';
+                    }
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
