b56e172968acc77d4abaaeb257f3e2f1600b3b4d
Ambuj Kumar <itsambuja@gmail.com>
2018-09-30 17:41:30 +0530

    students index page added

---
+++ CMS/views/manager/students/index.php
@@ -0,0 +1,28 @@
+<?php
+    include_once '../../../settings/db_connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+</head>
+<body>
+    <center>
+        <h1>Students panel</h1>
+    </center>
+    <hr>
+    <div>
+        <ul>
+            <li><a href="../../../index.php">Home</a></li>
+            <li><a href="student_list.php">Students list</a></li>
+            <li><a href="add_student.php">Add students</a></li>
+            <li><a href="remove_student.php">Update Students Info</a></li>
+            <li><a href="update_student.php">Remove Students</a></li>
+        </ul>
+    </div>
+</body>
+</html>
