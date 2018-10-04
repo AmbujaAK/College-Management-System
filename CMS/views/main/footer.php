980ec02c69610cebf4559344a4b58dde13e2cfae
Ambuj Kumar <itsambuja@gmail.com>
2018-10-04 15:15:39 +0530

    separate header and footer added

---
+++ CMS/views/main/footer.php
@@ -0,0 +1,20 @@
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Copyright © 2018 Company. All rights reserved. <a href="https://github.com/AmbujaAK">Ambuj</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../assets/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="../assets/build/js/custom.min.js"></script>
+	
+  </body>
+</html>
