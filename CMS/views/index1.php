043aee887940e8bb4bd5a9fafff1e1294c36a32b
Ambuj Kumar <itsambuja@gmail.com>
2018-10-04 15:13:50 +0530

    new home page added

---
+++ CMS/views/index1.php
@@ -0,0 +1,28 @@
+<?php
+    include 'main/header.php';
+?>
+<?php
+    include_once '../settings/db_connect.php';
+    $connected = "<span style='color:green'>Database connected !!</span>";
+    $not_connected = "<span style='color:red'>Database not connected !!</span>";
+?>
+
+<div class="right_col" role="main">
+    <center>
+        <h1>PHP Project</h1>
+        <?php if($conn) echo $connected; else echo $not_connected; ?>
+    </center>
+    <hr>
+    <div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="manager/students/">Student</a></li>
+            <li><a href="manager/teachers/">Teacher</a></li>
+            <li><a href="manager/departments/">Departments</a></li>
+        </ul>
+    </div>
+</div>
+
+<?php
+include 'main/footer.php'
+?>
